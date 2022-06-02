1f4d5f6a2b7664e62cd2aac96286af2446c08346
Mxtthi <matthias.keintzel@gmail.com>
2022-06-02 16:40:11 +0200

    small changes

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Snake browsergame
 
 - implement game settings into object
 - make "update method prettier"
-- adding behind still not working properly
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>2022 Project</title>
+    <title>Snake</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="stylesheet" href="style.css">
@@ -18,10 +18,10 @@
 </head>
 
 <body>
-	
-	<div>
-		<p id="highscore">Punkte: 0</p>
-	</div>
+
+    <div>
+        <p id="highscore">Punkte: 0</p>
+    </div>
 
     <?php
 
